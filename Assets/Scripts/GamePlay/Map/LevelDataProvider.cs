749438ea81943a08cb238d1d4bd87e57eb0da643
burakfdemir@gmail.com <burakfdemir@gmail.com>
2024-10-17 19:56:29 +0300

    Scriptable objects added, map build system added to scene.

---
+++ Assets/Scripts/GamePlay/Map/LevelDataProvider.cs
@@ -0,0 +1,25 @@
+﻿using GamePlay.TutorialStates;
+using UnityEngine;
+
+namespace GamePlay.Map
+{
+    public class LevelDataProvider : MonoBehaviour,ILevelDataProvider
+    {
+        [SerializeField] private LevelSO _levelData;
+        [SerializeField] private TutorialSo _tutorialData;
+        public ILevelData GetLevel()
+        {
+            return _levelData.LevelData;
+        }
+
+        public IMapData GetMapData()
+        {
+            return _levelData.LevelData;
+        }
+
+        public ITutorialData GetTutorialData()
+        {
+            return _tutorialData;
+        }
+    }
+}
